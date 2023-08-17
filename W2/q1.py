23788ca6311de6853c0d173835b3f49ac8e7e02a
sarthakgupta15 <sarthakgupta.univ@gmail.com>
2023-08-17 13:51:23 +0530

    Update q1.py

--- W2/q1.py
+++ W2/q1.py
@@ -1,8 +1,11 @@
+def duplicates(list):
+    l1=[]
+    for i in list:
+        if i not in l1:
+            l1.append(i)
+        else:
+            print(i,end=' ')
+
 l=[1,2,3,4,5,2,3,4,7,9,5]
-l1=[]
-for i in l:
-    if i not in l1:
-        l1.append(i)
-    else:
-        print(i,end=' ')
+duplicates(l)
 print("\n")
